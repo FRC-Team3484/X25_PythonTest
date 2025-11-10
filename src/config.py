dd1bd47f346e32a41bcedf8b4608a20c49ed872e
Purgso <jakob@spurgat.net>
2025-11-10 18:16:58 -0500

    feat: Added config

---
+++ src/config.py
@@ -0,0 +1,10 @@
+'''
+This file is used to enable/disable subsystems and various parts of the robot with a single line of code
+'''
+
+# Drivetrain and odometry
+DRIVETRAIN_ENABLED = True
+VISION_ENABLED = False
+
+# Disable All Commands
+COMMANDS_ENABLED = True
