2dc6e3f3274a57765ac7a873403fa785150800fa
Purgso <jakob@spurgat.net>
2025-11-22 22:05:01 -0500

    feat: moved pose functions to a separate module

--- src/FRC3484_Lib/pathfinding/pathfinding.py
+++ src/FRC3484_Lib/pathfinding/pathfinding.py
@@ -1,8 +1,7 @@
 from typing import Callable, Iterable
 
 from pathplannerlib.controller import PathFollowingController
-from robotpy_apriltag import AprilTagField, AprilTagFieldLayout
-from wpimath.geometry import Pose2d, Pose3d
+from wpimath.geometry import Pose2d
 import commands2
 from wpimath.units import inches, inchesToMeters
 
@@ -11,6 +10,7 @@ from pathplannerlib.auto import AutoBuilder
 from src.subsystems.drivetrain_subsystem import DrivetrainSubsystem
 from src.FRC3484_Lib.pathfinding.pathfinding_constants import PathfindingCommandConstants
 from src.FRC3484_Lib.pathfinding.final_alignment_command import FinalAlignmentCommand
+from src.FRC3484_Lib.pathfinding.apriltag_manipulation import get_nearest_pose
 
 class SC_Pathfinding:
     """
@@ -23,72 +23,10 @@ class SC_Pathfinding:
         - pose_supplier (DrivetrainSubsystem.poseSupplier): The pose supplier
         - april_tag_field_layout (AprilTagFieldLayout): The april tag field layout
     """
-    def __init__(self, drivetrain_subsystem: DrivetrainSubsystem, pose_supplier: Callable[[], Pose2d], april_tag_field: AprilTagField, drive_controller: PathFollowingController) -> None:
+    def __init__(self, drivetrain_subsystem: DrivetrainSubsystem, pose_supplier: Callable[[], Pose2d], drive_controller: PathFollowingController) -> None:
         self._drivetrain_subsystem: DrivetrainSubsystem = drivetrain_subsystem
         self._pose_supplier: Callable[[], Pose2d] = pose_supplier
-        self._april_tag_field_layout: AprilTagFieldLayout = AprilTagFieldLayout.loadField(april_tag_field)
         self._drive_controller: PathFollowingController = drive_controller
-
-    def get_april_tag_poses(self, april_tag_ids: Iterable[int]) -> list[Pose2d]:
-        """
-        Returns the poses of april tags by id
-
-        Parameters:
-            - april_tag_ids (list[int]): List of april tag ids
-
-        Returns:
-            - list[Pose2d]: The list of poses based on the specified ids
-        """
-        poses: list[Pose2d] = []
-
-        for id in april_tag_ids:
-            pose: Pose3d | None = self._april_tag_field_layout.getTagPose(id)
-            if pose is not None:
-                poses.append(pose.toPose2d())
-        
-        return poses
-
-    def apply_offset_to_pose(self, pose: Pose2d, offset: Pose2d) -> Pose2d:
-        """
-        Applies an offset to a pose
-
-        Parameters:
-            - pose (Pose2d): The pose to apply the offset to
-            - offset (Pose2d): The offset to apply
-
-        Returns:
-            - Pose2d: The resulting pose
-        """
-        return Pose2d(pose.translation() + offset.translation().rotateBy(pose.rotation()), pose.rotation() + offset.rotation())
-    
-    def apply_offsets_to_poses(self, poses: Iterable[Pose2d], offsets: Iterable[Pose2d]) -> list[Pose2d]:
-        """
-        Applies a list of offsets to a list of poses
-        Will return the number of offsets equal to poses times offsets
-        For example, if two poses and two offsets are provided, four poses will be returned
-
-        Parameters:
-            - poses (list[Pose2d]): The poses to apply the offsets to
-            - offsets (list[Pose2d]): The offsets to apply
-
-        Returns:
-            - list[Pose2d]: The resulting poses
-        """
-        return [self.apply_offset_to_pose(pose, offset) for pose in poses for offset in offsets]
-
-    def get_nearest_pose(self, poses: Iterable[Pose2d]) -> Pose2d:
-        """
-        Returns the nearest pose to the robot's current position
-
-        Parameters:
-            - poses (list[Pose2d]): The list of poses to find the nearest pose from
-
-        Returns:
-            - Pose2d: The nearest pose
-        """
-        if not type(poses) == list:
-            poses = list(poses)
-        return self._pose_supplier().nearest(poses)
 
     def get_final_alignment_command(self, target: Pose2d) -> commands2.Command:
         """
@@ -171,8 +109,8 @@ class SC_Pathfinding:
 
         if defer:
             return commands2.DeferredCommand(
-                lambda poses=poses: self.get_pathfind_command(self.get_nearest_pose(poses), distance, False), 
+                lambda poses=poses: self.get_pathfind_command(get_nearest_pose(self._pose_supplier(), poses), distance, False), 
                 self._drivetrain_subsystem
             )
         else:
-            return self.get_pathfind_command(self.get_nearest_pose(poses), distance, defer)
+            return self.get_pathfind_command(get_nearest_pose(self._pose_supplier(), poses), distance, defer)
